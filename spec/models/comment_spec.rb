f132b024b3417bbe489af0faa1d8ee83ebd9ca58
江原巧 <e.t.miiii228@gmail.com>
2021-04-22 19:00:10 +0900

    chore: rubocop の実行

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -26,13 +26,14 @@ RSpec.describe Comment, type: :model do
   let(:user) { create(:user, name: "NAME", email: "sample@example.com", password: "sample12") }
   let(:user_directory) { create(:user_directory, name: "user1", user_id: user.id) }
   let(:document) { create(:document, title: "Title", body: "Body", writer_id: user.id, owner: user, user_directory_id: user_directory.id) }
-  fcontext "body がある時" do
+  context "body がある時" do
     let(:comment) { build(:comment, document_id: document.id, user_id: user.id) }
     it "コメント登録される" do
       expect(comment).to be_valid
     end
   end
-  fcontext "body がない時" do
+
+  context "body がない時" do
     let(:comment) { build(:comment, body: nil, document_id: document.id, user_id: user.id) }
     it "エラーする" do
       expect(comment).not_to be_valid
