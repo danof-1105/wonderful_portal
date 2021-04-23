5473238a5ab9b7c08ad262c66ede08ba4fc7db8d
今永翔太 <>
2021-04-23 18:34:08 +0900

    test:Userモデルのテストを実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,5 +21,53 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションのチェック" do
+    subject { user }
+
+    context "必要な情報が入力されている時" do
+      let(:user) { create(:user) }
+      it "ユーザーが作成される" do
+        expect(subject).to be_valid
+      end
+    end
+
+    context "nameが入力されていない時" do
+      let(:user) { create(:user, name: nil) }
+      it "ユーザーが作成されない" do
+        expect(subject).not_to be_valid
+        expect(subject).to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context "emailが入力されていない時" do
+      let(:user) { create(:user, email: nil) }
+      it "ユーザーが作成されない" do
+        expect(subject).not_to be_valid
+        expect(subject).to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context "passwordが入力されていない時" do
+      let(:user) { create(:user, password: nil) }
+      it "ユーザーが作成されない" do
+        expect(subject).not_to be_valid
+        expect(subject).to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context "nameが20文字以下のとき" do
+      let(:user) { create(:user) }
+      it "ユーザーが作成される" do
+        expect(subject).to be_valid
+      end
+    end
+
+    context "nameが21文字以上のとき" do
+      let(:user) { create(:user, name: "A" * 21) }
+      it "ユーザーが作成されない" do
+        expect(subject).not_to be_valid
+        expect(subject).to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
 end
