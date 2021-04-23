1202bbe8cb2d1a5445ba892b636e2ed6e261f78a
今永翔太 <>
2021-04-23 18:22:08 +0900

    test:userモデルにfactorybotを追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -20,5 +20,8 @@
 #
 FactoryBot.define do
   factory :user do
+    name { Faker::Name.unique.first_name }
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 8) }
   end
 end
