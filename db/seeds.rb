637301491d067cf1f6e4ac232b52cc71d8a3f993
新田陸人 <>
2021-04-28 13:36:22 +0900

    faat: seedデータ作成の雛形を作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if User.all.blank?
+  ActiveRecord::Base.transaction do
+    puts "初期データの投入に成功しました！"
+  end
+else
+  puts <<~EOS
+    既にデータは存在しています。
+    下記コマンドを実行してデータを削除してから再読み込みしてください。
+    bundle exec rails db:migrate:reset
+  EOS
+end
