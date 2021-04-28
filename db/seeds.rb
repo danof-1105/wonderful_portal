1e4c290a358582cfc08b587d770160e6881c8f70
新田陸人 <>
2021-04-28 20:55:26 +0900

    fix: ヒアドキュメントの箇所をrubocop 通るように修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -127,9 +127,9 @@ if User.all.blank?
     puts "初期データの投入に成功しました！"
   end
 else
-  puts <<~EOS
+  puts <<~SQL
     既にデータは存在しています。
     下記コマンドを実行してデータを削除してから再読み込みしてください。
     bundle exec rails db:migrate:reset
-  EOS
+  SQL
 end
