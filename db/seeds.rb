03e123cf2b6ed0839d358d9db9299d82277bef1d
新田陸人 <>
2021-04-28 13:37:29 +0900

    feat: Userの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 if User.all.blank?
   ActiveRecord::Base.transaction do
+    ##########################################
+    # Userの作成
+    ##########################################
+    user = User.create!(email: "yamada@example.com", name: "山田 太郎", password: "password")
+    User.create!(email: "suzuki@example.com", name: "鈴木 次郎", password: "password")
+    User.create!(email: "satou@example.com", name: "佐藤 三郎", password: "password")
+
     puts "初期データの投入に成功しました！"
   end
 else
