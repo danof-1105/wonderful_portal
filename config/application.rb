e67efca5efda0c7672ebaff2f932efe893a3ff6a
万壽本大嗣 <manju.manju.03.28@gmail.com>
2021-04-29 16:59:45 +0900

    refactor: rubocop 実行

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module WonderfulPortal
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-   
+
     config.generators do |g|
       g.template_engine :erb
       g.assets fales
