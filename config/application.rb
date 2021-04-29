5ba0f844cb3fdc614415a5c7ca9867d03e3e230d
万壽本大嗣 <manju.manju.03.28@gmail.com>
2021-04-29 17:14:59 +0900

    fefactor: 不要なコメントアウト削除

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative "boot"
 
 require "rails"
-# Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
@@ -13,30 +12,17 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module WonderfulPortal
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+
     config.load_defaults 6.1
 
     # 標準言語とタイムゾーンを日本に変更
     config.i18n.default_locale = :ja
     config.time_zone = "Asia/Tokyo"
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Don't generate system test files.
 
     config.generators do |g|
       g.template_engine :erb
