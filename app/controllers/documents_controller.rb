7512216add32fbadd5673659f398ae322b1d4b42
新田陸人 <>
2021-05-04 16:49:07 +0900

    fix: rubucop を実行

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -5,7 +5,7 @@ class DocumentsController < ApplicationController
 
   def index
     @user_directories = UserDirectory.arrange
-    @documents = Document.all #仮で定義しているため後で削除
+    @documents = Document.all # 仮で定義しているため後で削除
   end
 
   def show
