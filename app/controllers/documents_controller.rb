98ccf72c7838dcaf40786403ceac797fef8a0013
山本大志 <taishi020294@gmail.com>
2021-05-07 18:53:31 +0900

    fix: rubocop実行

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -17,8 +17,8 @@ class DocumentsController < ApplicationController
     image = params[:document][:image_path]
     if image.present?
       document_image = DocumentImage.create!(document_image_params)
-      title =params[:document][:title]
-      body = params[:document][:body]<<("\n ![#{document_image.image_path}]")
+      title = params[:document][:title]
+      body = params[:document][:body] << ("\n ![#{document_image.image_path}]")
       document = current_user.documents.create!(title: title, body: body, owner: current_user, user_directory: user_directory)
     else
       document = current_user.documents.create!(document_params.merge(example))
@@ -26,14 +26,11 @@ class DocumentsController < ApplicationController
     redirect_to document, notice: "ドキュメントを登録しました。"
   end
 
-
   def document_params
     params.require(:document).permit(:title, :body)
   end
 
   def document_image_params
     params.require(:document).permit(:image_path)
-
   end
-
 end
