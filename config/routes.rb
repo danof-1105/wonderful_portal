1da5273fc5cfe465b5f9dbb032df3550adbc5690
山本大志 <taishi020294@gmail.com>
2021-05-09 00:22:59 +0900

    fix: だのふさんの指摘による修正①

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get "document_images/image"
   devise_for :users, skip: "passwords"
   resources :documents
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
