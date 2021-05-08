f5e1d41f252885d74b830553a60b704570e0e50a
江原巧 <e.t.miiii228@gmail.com>
2021-05-08 21:02:52 +0900

    WIP

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -16,13 +16,47 @@ class DocumentsController < ApplicationController
 
   def create
     # TODO: モック部、Devise実装後に修正必要
-    user_directory = current_user.user_directories.first
-    add_params = { owner: current_user, user_directory: user_directory }
-    @document = current_user.documents.create!(document_params.merge(add_params))
+    title_with_directory = params[:document][:title_with_directory].split("/")
+    title = title_with_directory[-1]
+    case title_with_directory.length
+    when 1
+      if current_user.user_directories.exists?(name: "no category")
+        add_params = { title: title, owner: current_user, user_directory: current_user.user_directories.find_by(name: "no category") }
+        @document = current_user.documents.create!(document_params.merge(add_params))
+      else
+        current_user.user_directories.create!(name: "no category")
+        add_params = { title: title, owner: current_user, user_directory: current_user.user_directories.find_by(name: "no category") }
+        @document = current_user.documents.create!(document_params.merge(add_params))
+      end
+    when 2
+      parents_directory = title_with_directory[0]
+      if current_user.user_directories.exists?(name: parents_directory)
+        add_params = { title: title, owner: current_user, user_directory: current_user.user_directories.find_by(name: parents_directory) }
+        @document = current_user.documents.create!(document_params.merge(add_params))
+      else
+        current_user.user_directories.create!(name: parents_directory)
+        add_params = { title: title, owner: current_user, user_directory: current_user.user_directories.find_by(name: parents_directory) }
+        @document = current_user.documents.create!(document_params.merge(add_params))
+      end
+    when 3
+      parents_directory = title_with_directory[0]
+      children_directory = title_with_directory[1]
+      if current_user.user_directories.exists?(name: parents_directory)
+        add_params = { title: title, owner: current_user, user_directory: current_user.user_directories.find_by(name: children_directory) }
+        @document = current_user.documents.create!(document_params.merge(add_params))
+      else
+        current_user.user_directories.create!(name: parents_directory)
+      end
+
+
+
+
+
+    end
     redirect_to @document, notice: "ドキュメントを登録しました。"
   end
 
   def document_params
-    params.require(:document).permit(:title, :body)
+    params.require(:document).permit(:title_with_directory, :body)
   end
 end
