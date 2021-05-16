7d54ac95947ed88e16a782abdf1f9a0ce99d10c0
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2021-05-16 16:20:48 +0900

    非同期での画像アップロード機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, skip: "passwords"
   root "documents#index"
   resources :documents
+  resources :document_images, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
