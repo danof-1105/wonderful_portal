31c1460172ca37ac8587ec4defbbb4670a1d8459
江原巧 <e.t.miiii228@gmail.com>
2021-05-17 23:05:52 +0900

    feat: CSRF 対策を off にする

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include ErrorHandler
 
+  protect_from_forgery with: :null_session
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
