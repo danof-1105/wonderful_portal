5beec0c850a79bf24d60755bcf854934d2bb6579
江原巧 <e.t.miiii228@gmail.com>
2021-05-17 23:05:18 +0900

    feat: routing の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, skip: "passwords"
   root "documents#index"
+  post '/member_joined_channel' => 'member_joined_channel#callback', as: :member_joined_channel
   resources :documents
   resources :document_images, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
