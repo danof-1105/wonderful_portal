648e092b0d564d2e3a8d46a9254853cd4314b5ad
江原巧 <e.t.miiii228@gmail.com>
2021-05-18 21:28:36 +0900

    feat: migration ファイルに confirmation 関係のカラムを追加し、rails db:migrate:reset を実行

--- db/schema.rb
+++ db/schema.rb
@@ -98,6 +98,10 @@ ActiveRecord::Schema.define(version: 2021_05_09_060919) do
     t.string "name", null: false
     t.string "image"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,9 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
 #  image                  :string
@@ -10,6 +13,7 @@
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
+#  unconfirmed_email      :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
