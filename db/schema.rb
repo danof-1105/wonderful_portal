a39aecd6c6e4f9dccdf81c7fcff587926ab117ac
江原巧 <e.t.miiii228@gmail.com>
2021-05-18 23:03:55 +0900

    feat: confirmation 関連の migration ファイルで更新漏れがあったので再更新

--- db/schema.rb
+++ db/schema.rb
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2021_05_09_060919) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
