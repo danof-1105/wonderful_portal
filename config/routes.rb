d91e07a7c9c71f09c715b3a531a30a053b8d2f40
江原巧 <e.t.miiii228@gmail.com>
2021-05-20 00:05:16 +0900

    feat: routeの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
   }
   root "documents#index"
+  post '/member_joined_channel' => 'member_joined_channel#callback', as: :member_joined_channel
   resources :documents
   resources :document_images, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
