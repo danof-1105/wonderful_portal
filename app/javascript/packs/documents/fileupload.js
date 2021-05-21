a5c64256c2ac558dff0d876b1026769a30db9a96
山本大志 <taishi020294@gmail.com>
2021-05-21 16:37:12 +0900

    feat/ドラッグ＆ドロップの実装

--- app/javascript/packs/documents/fileupload.js
+++ app/javascript/packs/documents/fileupload.js
@@ -1,20 +1,24 @@
-$("#image").change(() => {
-  const imageData = $("#image").prop("files")[0];
-  const formData = new FormData();
-  formData.append("image", imageData);
+$(function() {
+  $('#markdown_editor_textarea').on('drop', function(e) {
+    e.preventDefault();
+    f = e.originalEvent.dataTransfer.files[0];
+    var imageData = $('#image').prop('files', f);
+    var formData = new FormData();
+    formData.append("image", f);
 
-  $.ajax({
-    url: "/document_images",
-    type: "POST",
-    data: formData,
-    processData: false,
-    contentType: false,
-  }).done((data) => {
-    const url = data.url;
-    const filename = data.filename;
+    $.ajax({
+      url: "/document_images",
+      type: "POST",
+      data: formData,
+      processData: false,
+      contentType: false,
+    }).done((data) => {
+      const url = data.url;
+      const filename = data.filename;
 
-    const textArea = document.getElementById("markdown_editor_textarea");
-    const fileLink = `![${filename}](${url})`;
-    textArea.value = `${textArea.value}\n${fileLink}`;
+      const textArea = document.getElementById("markdown_editor_textarea");
+      const fileLink = `![${filename}](${url})`;
+      textArea.value = `${textArea.value}\n${fileLink}`;
+    });
   });
 });
