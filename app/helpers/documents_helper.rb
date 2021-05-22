d7b6e0ad95922124fbb1848b2aa9a27ed1c4945e
新田陸人 <>
2021-05-22 14:02:49 +0900

    refactor: ドキュメントタイトルのヘルパー呼び出し方法を修正

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -4,6 +4,10 @@ module DocumentsHelper
   end
 
   def placeholder_text
-    action_name == "new" ? "タイトル" : "#{@all_directories}/#{@title}"
+    action_name == "new" ? "タイトル" : ""
+  end
+
+  def value_text
+    action_name == "edit" ? "#{@all_directories}/#{@title}" : ""
   end
 end
