ad43ff8e631098f9552541c6b33d417afc743a6e
新田陸人 <>
2021-05-22 13:39:54 +0900

    refactor: ドキュメント編集時のタイトルにディレクトリ表示されるようにヘルパーを定義

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -2,4 +2,8 @@ module DocumentsHelper
   def button_text
     action_name == "new" ? "作成" : "更新"
   end
+
+  def placeholder_text
+    action_name == "new" ? "タイトル" : "#{@all_directories}/#{@title}"
+  end
 end
