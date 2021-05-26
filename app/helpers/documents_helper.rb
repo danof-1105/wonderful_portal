542f673ccbf942a338f8907af0f67be714320fb1
新田陸人 <>
2021-05-26 19:36:51 +0900

    fix: placaholder を削除

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,9 +1,5 @@
 module DocumentsHelper
 
-  def placeholder_text
-    action_name == "new" ? "タイトル" : ""
-  end
-
   def value_text
     action_name == "edit" ? "#{@all_directories}/#{@title}" : ""  # rubocop:disable  Rails/HelperInstanceVariable
   end
