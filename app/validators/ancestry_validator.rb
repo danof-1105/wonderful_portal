93ba1541ff15e7579c5698befbc2b52f7009ab4b
新田陸人 <>
2021-05-27 13:38:28 +0900

    fix: カスタムバリデーションのエラーメッセージを修正

--- app/validators/ancestry_validator.rb
+++ app/validators/ancestry_validator.rb
@@ -2,7 +2,7 @@ class AncestryValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     # HACK: /で判断させるのは直感的ではないため別の方法も検討する
     if value.count("/") > 3
-      record.errors.add(attribute, "ディレクトリは6つ以上作成できません！")
+      record.errors[:base] << "ディレクトリは5階層までしか作成できません"
     end
   end
 end
