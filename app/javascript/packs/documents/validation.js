57fc40428af55d78fdf135cafc5241d6503b828c
新田陸人 <>
2021-05-29 19:03:33 +0900

    refactor: バリデーションメッセージ表示の共通部分を変数化

--- app/javascript/packs/documents/validation.js
+++ app/javascript/packs/documents/validation.js
@@ -3,16 +3,17 @@ $(function() {
     let value = $(this).val();
     let directory = value.split("/")
     let title = directory.slice(-1)[0]
+    let validation = $(this).next();
 
     if (value == "" || !value.match(/[^\s\t]/)) {
-      $(this).next().text('タイトルを入力してください！');
+      validation.text('タイトルを入力してください！');
     } else if (title.length > 50 ) {
-      $(this).next().text('タイトルは50文字以内で入力してください！');
+      validation.text('タイトルは50文字以内で入力してください！');
       return false;
     } else if (directory.length > 6) {
-      $(this).next().text('ディレクトリは5つ以上作成できません！');
+      validation.text('ディレクトリは5つ以上作成できません！');
     } else {
-      $(this).next().text("");
+      validation.text("");
     }
 
     let result = false;
@@ -22,6 +23,6 @@ $(function() {
         return false;
       }
     });
-    if (result) { $(this).next().text('ディレクトリ名は20文字以内です'); }
+    if (result) { validation.text('ディレクトリ名は20文字以内です'); }
   });
 });
