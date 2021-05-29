4a9ba2fe16c41a6d61bb3a36d3e6f14e6a35fabf
新田陸人 <>
2021-05-29 18:59:15 +0900

    fix: タイトル部分のフォーカスが外れた際にメッセージが表示されるように設定

--- app/javascript/packs/documents/validation.js
+++ app/javascript/packs/documents/validation.js
@@ -1,16 +1,27 @@
 $(function() {
   $(".form-title").on("blur", function() {
-    let error;
     let value = $(this).val();
+    let directory = value.split("/")
+    let title = directory.slice(-1)[0]
 
     if (value == "" || !value.match(/[^\s\t]/)) {
-      error = true;
-    }
-
-    if (error) {
       $(this).next().text('タイトルを入力してください！');
+    } else if (title.length > 50 ) {
+      $(this).next().text('タイトルは50文字以内で入力してください！');
+      return false;
+    } else if (directory.length > 6) {
+      $(this).next().text('ディレクトリは5つ以上作成できません！');
     } else {
       $(this).next().text("");
     }
+
+    let result = false;
+    $.each(directory, function(index, value) {
+      if (value.length > 20) {
+        result = true;
+        return false;
+      }
+    });
+    if (result) { $(this).next().text('ディレクトリ名は20文字以内です'); }
   });
 });
