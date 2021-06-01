cb8225661a1ace7f2470e070795fc28ecf8b5bdb
新田陸人 <>
2021-06-01 13:42:08 +0900

    fix: エラーメッセージを配列に格納してまとめて出力できるように設定

--- app/javascript/packs/documents/validation.js
+++ app/javascript/packs/documents/validation.js
@@ -3,20 +3,24 @@ $(function() {
     const value = $(this).val();
     const directory = value.split("/")
     const title = directory.slice(-1)[0]
+    const validation_msg = []
     const validation = $("#title-error-message");
 
     // HACK: switch文に変更予定
     if (value == "" || !value.match(/[^\s\t]/)) {
-      validation.text('タイトルを入力してください！');
-    } else if (title.length > 50 ) {
-      validation.text('タイトルは50文字以内で入力してください！');
-      return false;
+      validation_msg.push('タイトルを入力してください！');
     } else if (directory.length > 6) {
-      validation.text('ディレクトリは5つ以上作成できません！');
+      validation_msg.push('ディレクトリは5つ以上作成できません！');
+    } else if (title.length > 50 ) {
+      validation_msg.push('タイトルは50文字以内で入力してください！');
+    }
+
+    $.each(directory, (index, value) => { if(value.length > 20) { validation_msg.push('ディレクトリ名は20文字以内です') } });
+
+    if (validation_msg.length > 0) {
+      validation.text(validation_msg);
     } else {
       validation.text("");
     }
-
-    $.each(directory, (index, value) => { if(value.length > 20) { validation.text('ディレクトリ名は20文字以内です') } });
   });
 });
