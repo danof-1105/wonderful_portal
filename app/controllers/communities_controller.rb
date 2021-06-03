07e9b6dbb3f5db3fa31f28ec896f01ff417c63b9
江原巧 <e.t.miiii228@gmail.com>
2021-06-03 14:42:14 +0900

    feat: ページネーション機能の追加

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,6 +1,8 @@
 class CommunitiesController < ApplicationController
+  PER_PAGE = 10
 
   def index
     @communities = Community.all
+    @communities = @communities.page(params[:page]).per(PER_PAGE)
   end
 end
