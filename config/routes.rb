1c6c1d1d5e9c5ec6f02a2a104d2beab5eafd6e0b
江原巧 <e.t.miiii228@gmail.com>
2021-06-03 13:02:36 +0900

    feat: communities controller の作成と routing の設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   root "documents#index"
   resources :documents
   resources :document_images, only: [:create]
+  resources :communities
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
