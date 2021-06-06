9061c113dfef9c098f228b124fe49b377b918a55
江原巧 <e.t.miiii228@gmail.com>
2021-06-06 23:50:59 +0900

    feat: 指摘事項の修正

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -2,7 +2,7 @@ class CommunitiesController < ApplicationController
   PER_PAGE = 10
 
   def index
-    @communities = Community.all
+    @communities = current_user.communities
     @communities = @communities.page(params[:page]).per(PER_PAGE)
   end
 end
