2726071aaee02371d0015c574920e1b533425294
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-06-14 22:01:26 +0900

    feat: オーナーとしてコミュニティを作成できるよう実装

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -5,4 +5,11 @@ class CommunitiesController < ApplicationController
     @communities = current_user.communities
     @communities = @communities.page(params[:page]).per(PER_PAGE)
   end
+
+  def create
+    @community = current_user.communities.create!(name: params[:name], owner: current_user)
+
+    flash[:success] = "#{@community.name}コミュニティーを作成しました。"
+    redirect_back(fallback_location: root_path)
+  end
 end
