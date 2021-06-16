a86c83f33edb7e2725858eccafe99d3c94ba5ca1
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-06-16 21:22:06 +0900

    feat: コミュニティ管理画面からコミュニティ作成しようとしいる

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,12 +1,17 @@
 class CommunitiesController < ApplicationController
   PER_PAGE = 10
 
+  def new
+    # TODO: コミュニティ管理画面
+  end
+
   def index
     @communities = current_user.communities
     @communities = @communities.page(params[:page]).per(PER_PAGE)
   end
 
   def create
+    # FIXME: 重複コミュニティバリエーション or "既に存在するコミュニティ名です" の文言
     @community = current_user.communities.create!(name: params[:name], owner: current_user)
 
     flash[:success] = "#{@community.name}コミュニティーを作成しました。"
